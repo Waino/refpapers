84d3176686c0c5c61f0a9e592244f3285353b94e
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2021-07-24 22:13:31 +0300

    First phase of integration test

--- tests/integration.sh
+++ tests/integration.sh
@@ -2,15 +2,37 @@
 
 set -eu
 
-TESTING_DIR=$(mktemp --directory papers2.XXXXXXXXXX)
+TESTING_DIR=$(mktemp --directory refpapers.XXXXXXXXXX --tmpdir=/tmp)
+CONF_DIR="${TESTING_DIR}/conf"
 INDEX_DIR="${TESTING_DIR}/index"
 DATA_DIR="${TESTING_DIR}/data"
 
 pushd "${TESTING_DIR}"
 echo "Performing integration test in ${TESTING_DIR}"
 
-#FIXME
+### Set up
+mkdir -p "${CONF_DIR}"
+mkdir -p "${INDEX_DIR}"
+mkdir -p "${DATA_DIR}/topicA"
+mkdir -p "${DATA_DIR}/topicB"
+# some files to index
+touch "${DATA_DIR}/topicA/A_-_T_2021.pdf"
+touch "${DATA_DIR}/topicA/Author_-_Title_book_2021.pdf"
+touch "${DATA_DIR}/topicB/Author_-_MoreTitle_survey_2020.pdf"
+# dummy version forces reindexing
+echo "dummy-version" > ${CONF_DIR}/schema_version
+
+echo ""
+echo "### Create a minimal conf, perform full indexing"
+echo "'Extraction failed' messages are ok: dummy files are empty"
+echo "refpapers index --confdir ${CONF_DIR}"
+refpapers index --confdir "${CONF_DIR}" <<CREATEMINIMAL
+y
+${DATA_DIR}
+${INDEX_DIR}
+CREATEMINIMAL
 
 popd
-rm -r "${TESTING_DIR}"
+#rm -r "${TESTING_DIR}"
+echo ""
 echo "Integration test ran to completion"
