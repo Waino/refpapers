f048eb6579138d42ae5e882d29f23f986df1a276
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2021-07-24 21:50:59 +0300

    entrypoint is cli, not main

--- refpapers/__init__.py
+++ refpapers/__init__.py
@@ -2,13 +2,14 @@
 """
 refpapers - FIXME
 """
+from refpapers.refpapers import cli
 from refpapers.view import print_list, print_details
 from refpapers.filesystem import yield_all_paths, parse
 from refpapers.schema import Paper
 from refpapers.search import index_data, search
 
 __all__ = ['print_list', 'print_details', 'yield_all_paths', 'parse',
-           'Paper', 'index_data', 'search']
+           'Paper', 'index_data', 'search', 'cli']
 
 __version__ = '0.0.1'
 __author__ = 'Stig-Arne Gronroos'
