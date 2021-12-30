48a3a22ec909896260ff7e80b005523acb6488d6
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2021-12-30 13:56:50 +0200

    Google Scholar API

--- refpapers/apis.py
+++ refpapers/apis.py
@@ -1,5 +1,6 @@
 from crossref.restful import Works  # type: ignore
 from pathlib import Path
+from scholarly import scholarly, ProxyGenerator  # type: ignore
 from typing import Optional, List
 import arxiv  # type: ignore
 import os
@@ -119,4 +120,64 @@ class ArxivApi:
                 return None
 
         meta = self._cache.get(id, _uncached_fetch)
+        return meta
+
+
+class ScholarApi:
+    def __init__(self, conf: Conf):
+        if conf.paths.api_cache:
+            os.makedirs(conf.paths.api_cache, exist_ok=True)
+            cache_dir = conf.paths.api_cache
+        else:
+            cache_dir = Path('/tmp')
+        self._cache = JsonFileCache(cache_dir / 'scholar.jsonl')
+        self._proxy_setup()
+
+    def metadata_from_title(self, title: str, path=None) -> Optional[Paper]:
+        meta = self._fetch(title)
+
+        try:
+            title = meta['bib']['title']
+            year = int(meta['bib']['pub_year'])
+            authors = [author.split()[-1] for author in meta['bib']['author']]
+            # url = meta.get('eprint_url', None)
+        except AttributeError:
+            return None
+        except ValueError:
+            return None
+
+        if not all((title, year, authors)):
+            return None
+
+        bibtex = BibtexKey(authors[0], year, BibtexKey.title_word(title))
+
+        return Paper(
+            path=path,
+            bibtex=bibtex,
+            title=title,
+            authors=authors,
+            year=year,
+            pub_type=[],
+            tags=[],
+            number=None,
+            doi=None,
+            arxiv=None,
+        )
+
+    def _proxy_setup(self):
+        pg = ProxyGenerator()
+        pg.FreeProxies()
+        scholarly.use_proxy(pg)
+
+    def _fetch(self, title: str):
+        """ Returns metadata or None if id not found """
+
+        def _uncached_fetch():
+            try:
+                results = scholarly.search_pubs(title)
+                return next(results)
+            except Exception:
+                return None
+
+        meta = self._cache.get(title, _uncached_fetch)
         return meta
