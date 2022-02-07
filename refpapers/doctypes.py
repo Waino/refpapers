3df33819340f48163c1097d98a68283fd01e3d1c
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2022-02-07 13:41:37 +0200

    doctypes.py

---
+++ refpapers/doctypes.py
@@ -0,0 +1,14 @@
+import delegator  # type: ignore
+import os
+from pathlib import Path
+
+from refpapers.conf import Conf
+
+
+def open_in_viewer(path: Path, conf: Conf):
+    _, ending = os.path.splitext(path)
+    if not ending:
+        print(f'Unable to determine ending of path "{path}"')
+        return
+    viewer = conf.software.get_viewer(ending)
+    delegator.run(f'{viewer} {path}', block=False)
