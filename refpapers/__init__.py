44a229e835338b5294778f1cdff0267e5c63ccd0
Stig-Arne Grönroos <stig.gronroos@gmail.com>
2023-12-03 15:52:09 +0200

    Release v1.1.2

--- refpapers/__init__.py
+++ refpapers/__init__.py
@@ -11,6 +11,6 @@ from refpapers.search import index_data, search
 __all__ = ['print_list', 'print_details', 'yield_all_paths', 'parse',
            'Paper', 'index_data', 'search', 'cli']
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'Stig-Arne Gronroos'
 __author_email__ = "stig.gronroos@gmail.com"
